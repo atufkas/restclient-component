9b2797e229d33bb402456aa0115b52a226c344e6
nilopc <nilopc@gmail.com>
2013-08-20 21:13:09 +0200

    Added LICENSE

--- src/NilPortugues/Component/RestfulClient/Interfaces/RestfulClientInterface.php
+++ src/NilPortugues/Component/RestfulClient/Interfaces/RestfulClientInterface.php
@@ -1,4 +1,10 @@
 <?php
+/*
+ * Author: Nil Portugués Calderó <contact@nilportugues.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 namespace NilPortugues\Component\RestfulClient\Interfaces;
 
 interface RestfulClientInterface
--- src/NilPortugues/Component/RestfulClient/Responses/JSONResponse.php
+++ src/NilPortugues/Component/RestfulClient/Responses/JSONResponse.php
@@ -1,4 +1,10 @@
 <?php
+/*
+ * Author: Nil Portugués Calderó <contact@nilportugues.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 namespace NilPortugues\Component\RestfulClient\Responses;
 
 use  NilPortugues\Component\RestfulClient\Responses\RestfulResponseInterface as RestfulResponseInterface;
