46ba505f2aa30ebfef4ec1f314c6e8ddbf33dd17
Matthias Lienau <matthias@mlienau.de>
2014-06-19 17:55:09 +0200

    Fixed notice caused by uninitialized array for responses with Content-Type application/json.

--- src/Sonrisa/Component/RestfulClient/AbstractClient.php
+++ src/Sonrisa/Component/RestfulClient/AbstractClient.php
@@ -24,15 +24,39 @@ abstract class AbstractClient
             switch ($type) {
                 case 'application/json':
                     $data = Responses\JSONResponse::convert($data);
-                    $status = explode(' ',$data['headers']['Status']);
-                    $data['headers']['Status'] = $status[0];
+
+                    // NOTE:
+                    //
+                    // Next block causes a "Notice" and is quite confusing - probably
+                    // a "typo for historical reasons" and might be removed completely soon:
+                    //
+                    // $data is response body data and therefore not a subject to be parsed
+                    // for "header" fields. In case $responseHeaders was meant instead, it's
+                    // still unclear why the status code should be added to the $data array
+                    // which should only provide parsed data based on detected Content-Type
+                    // as returned by this method.
+                    //
+                    // It's now put into an existence check instead of actually removing it
+                    // to maintain BC compatibility for the unlikely case somebody used that
+                    // as a "feature".
+                    //
+                    if (isset($data['headers']['Status'])) {
+                        $status = explode(' ',$data['headers']['Status']);
+                        $data['headers']['Status'] = $status[0];
+                    }
+
                     return $data;
                     break;
 
                 case 'application/xml':
                     $data = Responses\XMLResponse::convert($data);
-                    $status = explode(' ',$data['headers']['Status']);
-                    $data['headers']['Status'] = $status[0];
+
+                    // NOTE: (see comment above for case "application/json")
+                    if (isset($data['headers']['Status'])) {
+                        $status = explode(' ',$data['headers']['Status']);
+                        $data['headers']['Status'] = $status[0];
+                    }
+
                     return $data;
                     break;
 
